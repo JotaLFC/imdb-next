2da0f1c0cda6296ff17c2a9e2377fefd8004b37e
Insy Hernsvan <insy.hernsvan@envisionpharma.com>
2025-02-06 14:47:51 -0500

    wrap suspense around navbaritem

--- src/components/NavbarItem.jsx
+++ src/components/NavbarItem.jsx
@@ -1,23 +1,56 @@
+// 'use client'
+
+// import Link from 'next/link'
+// import { useSearchParams } from 'next/navigation';
+
+// export default function NavbarItem({title, param}) {
+//     const searchParams = useSearchParams();
+//     const genre = searchParams.get('genre');
+//   return (
+//     <div>
+//       <Link
+//         className={`hover:text-amber-600 font-semibold ${
+//           genre === param
+//             ? 'underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg'
+//             : ''
+//         }`}
+//         href={`/?genre=${param}`}
+//       >
+//         {title}
+//       </Link>
+//     </div>
+//   )
+// }
+
 'use client'
 
 import Link from 'next/link'
 import { useSearchParams } from 'next/navigation';
+import { Suspense } from 'react';
 
-export default function NavbarItem({title, param}) {
-    const searchParams = useSearchParams();
-    const genre = searchParams.get('genre');
+function NavbarItemContent({ title, param }) {
+  const searchParams = useSearchParams();
+  const genre = searchParams.get('genre');
+  return (
+    <Link
+      className={`hover:text-amber-600 font-semibold ${
+        genre === param
+          ? 'underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg'
+          : ''
+      }`}
+      href={`/?genre=${param}`}
+    >
+      {title}
+    </Link>
+  );
+}
+
+export default function NavbarItem({ title, param }) {
   return (
     <div>
-      <Link
-        className={`hover:text-amber-600 font-semibold ${
-          genre === param
-            ? 'underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg'
-            : ''
-        }`}
-        href={`/?genre=${param}`}
-      >
-        {title}
-      </Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <NavbarItemContent title={title} param={param} />
+      </Suspense>
     </div>
-  )
+  );
 }
